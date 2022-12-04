41dbf928608cfe109a1bd9ad01fddb57aa2bf9e2
jryjson <jerryjeduc@gmail.com>
2022-12-04 10:41:54 +0100

    En commit som användare 2

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -2,6 +2,7 @@ package com.demo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello world as new user!");
     }
 }
