4f627535dc2b379d4ea3371b7cb4f02d40f38845
jryjson <jerryjeduc@gmail.com>
2022-12-04 10:44:26 +0100

    Test i intellij

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world as new user!");
+        System.out.println("Git Test i Intellij!");
     }
 }
